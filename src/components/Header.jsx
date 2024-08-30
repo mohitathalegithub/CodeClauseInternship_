34e042f7f046831ea019a183f254ba69c40b669c
mohit athale <mohitathale11@gmail.com>
2024-08-30 15:58:56 +0530

    addes dsome code in cards comp

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import '../App.css';
 function Header(props) {
     return (
         <div className='flex shopping-card'>
+            <h1>shopping Cart</h1>
             <div onClick={() => props.handleShow(false)} >Shopping Cart App</div>
             <div onClick={() => props.handleShow(true)}> Cart
                 <sup> {props.count} </sup>
